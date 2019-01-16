18ace74696403509f7d1de7b4d5c57d88dda07d0
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-16 21:42:48 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,10 @@
 
 def valid_move?(board, index)
-  board[index + 1] == index.between?(1, 9) && position_taken?(board, index)  ? false : true
-  
-end
+  index.between?(0, 8) && position_taken?(board, index) 
+  end
 
 def position_taken?(board, index)
   board[index] == "" || board[index] == " " || board[index] == nil ? false : true
 end
+
+
